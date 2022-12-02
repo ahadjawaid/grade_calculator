80fc730b10577215b0cfbb54a1719e993c3f31dc
Ahad Jawaid <ahadjawaid0@gmail.com>
2022-12-02 10:57:57 -0600

    Initial Commit

---
+++ calculator.py
@@ -0,0 +1,56 @@
+class Grades:
+    def __init__(self, grading_scale, scores, scores_weight):
+        self.scores = scores
+        self.scores_weight = scores_weight
+        self.grading_scale = grading_scale
+        self.points = self._getCurrentPoints()
+        self.letter_grade = self._getLetterGradeWithPoints(self.points)
+
+    def _getCurrentPoints(self):
+        current_points = 0
+        for key in self.scores:
+            for i in range(len(self.scores[key])):
+                current_points += self.scores[key][i] * self.scores_weight[key][i]
+        
+        return current_points
+
+    def _getValueForLetter(self, letter):
+        for key in self.grading_scale:
+            if self.grading_scale[key] == letter:
+                return key     
+
+    def _getLetterGradeWithPoints(self, points):
+        range_values = sorted(list(self.grading_scale.keys()))
+        range_val = max(range_values)
+
+        for i in range(1, len(range_values)):
+            if points < range_values[i]:
+                return  self.grading_scale[range_values[i-1]]
+                
+        return self.grading_scale[range_val]
+
+    def getScoreNeededForItem(self, key, index, goal):
+        item_weight = self.scores_weight[key][index]
+        goal_points = self._getValueForLetter(goal)
+        points_needed = goal_points - self.points
+        score_needed = points_needed / item_weight
+
+        return score_needed
+    
+    def letterGradesPossibleForItem(self, key, index):
+        score_needed = {}
+        for letter in self.grading_scale.values():
+            score = self.getScoreNeededForItem(key, index, letter)
+            if 0 <= score <= 100:
+                score_needed[letter] = score
+
+        return score_needed
+
+# my_grades = Grades(scores, scores_weight, grading_scale)
+
+# current_letter_grade = my_grades.letter_grade
+
+# scores_for_exam_3 = my_grades.letterGradesPossibleForItem("exam", 2)
+
+# for letter in scores_for_exam_3:
+#     print(f"Score of {round(scores_for_exam_3[letter], 2)} to get a {letter}")
---
+++ main.py
@@ -0,0 +1,62 @@
+from calculator import Grades
+import argparse
+import json
+import os
+
+def getConfig(config_path):
+    with open(config_path, "r") as f:
+        config = json.load(f)
+
+    return config
+
+def preprocessConfig(config):
+    for class_config in list(config["class"]):
+        for grade_value in list(config["class"][class_config]["grading_scale"]):
+            config["class"][class_config]["grading_scale"][float(grade_value)] = (
+                config["class"][class_config]["grading_scale"][grade_value])
+            del config["class"][class_config]["grading_scale"][grade_value]
+
+    return config
+
+def classInConfig(class_name, config):
+    if not config["class"][class_name]:
+        raise Exception(f"Class: '{class_name}' was not found in the config.")
+    else:
+        return True
+
+def printScoreNeeded(item):
+    scores_needed = user_grades.letterGradesPossibleForItem(*item)
+    print(f"Grades needed for {item[0].capitalize()} {item[1] + 1}:")
+    for letter in scores_needed:
+        print(f"Score of {round(scores_needed[letter], 2)} to get a {letter}")
+
+# Setup arg parser
+parser = argparse.ArgumentParser(prog = 'Grade calculator')
+parser.add_argument('-c', '--class_name')           # Class name defined in config
+parser.add_argument('-l', '--get_letter_grade',     # Flag to print current letter grade
+                    action="store_true")
+parser.add_argument('-s', '--score_needed')         # Input item needed Ex: 'exam 3'
+# Get arguments
+args = parser.parse_args()
+class_name = args.class_name.strip()
+get_letter_grade_flag = bool(args.get_letter_grade)
+score_needed_item = args.score_needed.split(" ")
+
+# Import config
+config_path = os.path.join(os.getcwd(), "config.json")
+config = getConfig(config_path)
+
+# Validate argument is in config
+classInConfig(class_name, config)
+config = preprocessConfig(config)
+
+class_config = config["class"][class_name]
+user_grades = Grades(class_config["grading_scale"], class_config["scores"], class_config["scores_weight"])
+
+if get_letter_grade_flag:
+    print(f"Current letter grade: {user_grades.letter_grade}")
+
+if score_needed_item:
+    item = score_needed_item
+    item[1] = int(item[1]) - 1
+    printScoreNeeded(item)
